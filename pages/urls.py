5ea3088fbc9d64f475f1bd8f7d9a3991b5039294
pgupta8825 <princegupta672000@gmail.com>
2021-04-03 12:56:54 +0530

    inital commit

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import homePageView
+#from .views import HomePageView
+from .views import HomePageView, AboutPageView # new
 urlpatterns = [
-path('', homePageView, name='home')
+path('', HomePageView.as_view(), name='home'),
+path('about/', AboutPageView.as_view(), name='about'), # new
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+from django.views.generic import TemplateView
+
 from django.http import HttpResponse
 def homePageView(request):
-    return HttpResponse('Hello, World!')
+    return HttpResponse('Hello, World!'),
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
